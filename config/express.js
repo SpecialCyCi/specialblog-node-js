90681dd6e6cee0b4298fa8fbece6be2158e50159
Special Leung <specialcyci@gmail.com>
2014-11-19 15:42:27 +0800

    byte byte jade...

--- config/express.js
+++ config/express.js
@@ -8,7 +8,8 @@ var express = require('express'),
 module.exports = function(app, config) {
   app.set('port', config.port);
   app.set('views', config.root + '/app/views');
-  app.set('view engine', 'jade');
+  app.engine('html', require('ejs').renderFile);
+  app.set('view engine', 'html');
   app.use(favicon(config.root + '/public/img/favicon.ico'));
   app.use(morgan('dev'));
   app.use(methodOverride());
