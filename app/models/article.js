e0d4a0d6ec63e86a9c02943f6c43ca5aee75c7a7
Special Leung <specialcyci@gmail.com>
2014-11-29 22:28:34 +0800

    Add created_at updated_at to article.

--- app/models/article.js
+++ app/models/article.js
@@ -10,12 +10,22 @@ var ArticleSchema = new Schema({
     required: 'Title cannot be blank.'
   },
   content: String,
-  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }], 
+  created_at: { type: Date }, 
+  updated_at: { type: Date }
 });
 
 ArticleSchema.virtual('date')
   .get(function(){
     return this._id.getTimestamp();
+  })
+  .pre('save', function(next){
+    now = new Date();
+    this.updated_at = now;
+    if ( !this.created_at ) {
+        this.created_at = now;
+      }
+    next();
   });
 
 mongoose.model('Article', ArticleSchema);
